448f631f0f1c5db8782581b6a628cf63efaaa492
Amol kapadi <amolakapadi@gmail.com>
2024-09-13 18:39:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; 
 import './App.css';
 
-function App() {
+const App = () => {
+  const [value, setValue] = useState(''); 
+  const [notes, setNotes] = useState([]); 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setNotes([...notes, value]);
+    setValue('');
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="conatiner">
+      <h2>React Quill Notes Application</h2>
+      <ReactQuill 
+        theme="snow" 
+        value={value} 
+        onChange={setValue} 
+        className="custom-editor" 
+      />
+      <button onClick={handleSubmit} className='add-btn' disabled={!value.trim()}>
+        Add Note
+      </button>
+      <div style={{ marginTop: '20px' }}>
+        <h3>Your Notes:</h3>
+        {notes.length === 0 && <p>No notes yet.</p>}
+        <ul>
+          {notes.map((note, index) => (
+            <li key={index} style={{ marginBottom: '20px'}}>
+              <div dangerouslySetInnerHTML={{ __html: note }} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
